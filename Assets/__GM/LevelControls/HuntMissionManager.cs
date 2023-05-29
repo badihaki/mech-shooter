c92730f01ac4d16387aaec15b6bd4ec535a3aed4
badihaki <jojackblack@gmail.com>
2023-05-29 15:36:39 -0400

    Initialized git repo

---
+++ Assets/__GM/LevelControls/HuntMissionManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class HuntMissionManager : MonoBehaviour
+{
+    private GameObject gm;
+
+    [Header("Hold the score, show the score goal")]
+    [SerializeField] private int score;
+    [SerializeField] private int scoreGoal;
+    [Header("Set the mininum and maximum score")]
+    public int scoreMin = 35;
+    public int scoreMax = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreGoal = UnityEngine.Random.Range(25, 75);
+
+        gm = GameObject.Find("Game Master");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateUI();
+
+    }
+
+    private void UpdateUI()
+    {
+        transform.Find("Score").Find("ScoreText").GetComponent<TMPro.TextMeshProUGUI>().text = score.ToString() + " / " + scoreGoal.ToString();
+    }
+
+    public void AddToScore()
+    {
+        score += 1;
+        if (score >= scoreGoal)
+        {
+            // win state in mission manager
+            GetComponent<MissionManager>().WinState();
+        }
+    }
+
+}
---
+++ Assets/__Scenes/Mission Select/Missions/MissionSO.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Mission",menuName ="AreaX-00")]
+public class MissionSO : ScriptableObject
+{
+    [Header("Mission Locks")]
+    public bool missionUnlocked; // if the mission is unlocked, it is available
+    public bool missionComplete; // if the mission is completed
+    public bool unlockablePilot; // if there is an unlockable pilot
+    public Pilot_SO pilotUnlock;
+    public bool unlockableMecha; // if there is an unlockable mecha
+    public Mecha_SO mechaUnlock;
+    public bool unlockableWeapon;
+    public Weapon_SO weaponUnlock;
+
+    public enum FirstMissionType
+    {
+        elimination,
+        survival,
+        chase,
+        escort,
+    }
+    
+    [Header("Mission Types")]
+    public FirstMissionType initialMission;
+    public bool missionOneHasBoss;
+    public GameObject[] minibossPool;
+    // ^^^ All of this is for Mission 1
+    // If Mission 1 has a boss, Mission 2 should be a boss fight
+    // If Mission 1 has another mission, specify it
+    public enum SecondMisionType
+    {
+        nada,
+        elimination,
+        survival,
+        chase,
+        escort,
+        boss,
+    }
+    public SecondMisionType secondMision;
+    public bool missionTwoHasBoss;
+    public GameObject[] bossPool;
+    // ^^^ All of this for Mission 2
+    // If Mission 2 has a boss, fuckit, throw a boss in there
+
+    [Header("UI Stuff")]
+    public string missionTitle;
+    public Sprite openingImage;
+    public string missionInfo;
+
+    public void UnlockMission()
+    {
+        missionUnlocked = true;
+        Debug.Log("new mission unlocked, " + missionTitle);
+    }
+
+    public void MissionCompleted()
+    {
+        missionComplete = true;
+        if (unlockablePilot == true)
+        {
+            GameObject.Find("Game Master").GetComponent<PilotCrew>().AddPilot(pilotUnlock);
+        }
+        if (unlockableMecha == true)
+        {
+            GameObject.Find("Game Master").GetComponent<MechHangar>().AddNewMechaToHangar(mechaUnlock);
+        }
+        if (unlockableWeapon == true)
+        {
+            GameObject.Find("Game Master").GetComponent<MechHangar>().AddNewMechaToHangar(mechaUnlock);
+        }
+    }
+}
